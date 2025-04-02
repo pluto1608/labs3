a593be17725cdf846c52da5e10a71ba147c6c306
pluto <84376137+pluto1608@users.noreply.github.com>
2025-04-02 22:01:02 +0000

    podsreden main i iskomentirano

--- labs32.cpp
+++ labs32.cpp
@@ -9,19 +9,19 @@ struct jazol4e{
 
 struct listi4ka{
   jazol4e *shef;
-  jazol4e *gz;
-  void init(){shef=gz=nullptr;}
-  void pishiPo(jazol4e *p,int n){
+  jazol4e *gz;//pokazuvac kon posledniot jazol
+  void init(){shef=gz=nullptr;}//isto kako edinecno povrzana samo sega ima i pointer za posledniot clen
+  void pishiPo(jazol4e *p,int n){//dodava jazol posle konkreten jazol
       jazol4e *nov=new jazol4e;
       nov->podato4e=n;
       nov->pret=p;
-      nov->sled=p->sled;
-      if(p->sled==nullptr)gz=nov;
-      else p->sled->pret=nov;
-      p->sled=nov;
+      nov->sled=p->sled;//se povrzuva pointerot sled na noviot jazol so jazolot koj e posle jazolot koj veke postoi
+      if(p->sled==nullptr)gz=nov;//dokolku postoeckiot jazol bil posleden gz da pokazuva kon nego
+      else p->sled->pret=nov;//pointerot pret na jazolot koj bil po p se stava da pokazuva kon noviot
+      p->sled=nov;//p da pokazuva kon noviot jazol
   }
-  void pishiPre(jazol4e *p,int n){
-    jazol4e *nov=new jazol4e;
+  void pishiPre(jazol4e *p,int n){//dodava jazol pred konkreten jazol(slicno so pishiPo me mrzi da objasnam)
+    jazol4e *nov=new jazol4e;//pishete ako neso konkretno ne e jasno :)
     nov->podato4e=n;
     nov->sled=p;
     nov->pret=p->pret;
@@ -29,16 +29,17 @@ struct listi4ka{
     else p->pret->sled=nov;
     p->pret=nov;
   }
-  void pishiPrv(int n){
+  void pishiPrv(int n){//dodava prv jazol slicno so drugite pishi
     jazol4e *nov=new jazol4e;
     nov->podato4e=n;
-    nov->pret=nullptr;
-    nov->sled=shef;
+    nov->pret=nullptr;//zaso e prv jazol pret pointerot pokazuva kon nullptr
+    nov->sled=shef;//nov sled NE POKAZUVA KON SHEF shef e pokazuvac kon prviot jazol nov->sled sega pokazuva
+    //kon jazolot shto prethodno bil prv
     if(shef==nullptr)gz=nov;
     else shef->pret=nov;
     shef=nov;
   }
-  void pishiPosl(int n){
+  void pishiPosl(int n){//slicno so pishi prv
     jazol4e *nov=new jazol4e;
     nov->podato4e=n;
     nov->sled=nullptr;
@@ -47,7 +48,7 @@ struct listi4ka{
     else gz->sled=nov;
     gz=nov;
   }
-  void brishiPosl(){
+  void brishiPosl(){//skoro isto so kaj edinecno povrzana
     if(shef!=nullptr){
       if(shef->sled==nullptr){
         delete shef;
@@ -87,35 +88,33 @@ struct listi4ka{
 };
 
 void sort(listi4ka &l) {
-  if (l.shef == nullptr || l.shef->sled == nullptr) return;
+  if (l.shef == nullptr || l.shef->sled == nullptr) return;//za ako nema ili ima samo eden clen
 
   bool swapped;
-  do {
+  do {//bubble sort ama pokomplicirano zasho e povrzana lista
     swapped = false;
     jazol4e *current = l.shef;
 
-    while (current->sled != nullptr) {
-      if (current->podato4e > current->sled->podato4e) {
+    while (current->sled != nullptr) {//vrti niz celata lista
+      if (current->podato4e > current->sled->podato4e) {//ako podatokot na tekovniot jazol e pogolen od podatokot na sledniot
         jazol4e *next = current->sled;
         if (current->pret != nullptr)
-          current->pret->sled = next;
+          current->pret->sled = next;//pokazuvacot sled na jazolot pred tekovniot da pokazuva kon sledniot jazol
         else
-          l.shef = next;
+          l.shef = next;//ako nema jazol pred tekovniot shef se menuva
 
         if (next->sled != nullptr)
-          next->sled->pret = current;
+          next->sled->pret = current;//pokazuvacot pret na jazolot posle sledniiot da pokazuva kon tekovniot
         else
-          l.gz = current;
-
-
-        current->sled = next->sled;
-        next->pret = current->pret;
-        current->pret = next;
-        next->sled = current;
-
-        swapped = true;
+          l.gz = current;//ako nema jazol posle sledniot gz se menuva
+        current->sled = next->sled;//sled da pokazuva kon jazolot posle sledniot
+        next->pret = current->pret;//pret na sledniot da pokazuva kon jazolot pred tekovniot
+        current->pret = next;//tekovniot da pokazuva kon sledniot
+        next->sled = current;//sledniot da pokazuva kon tekovniot
+        swapped = true;//koga nizata e sortirana nema da bide ispolnet ovoj if uslov i swapped ke ostane false  
       } else {
-        current = current->sled;
+        current = current->sled;//ako ne se menuvaat se odi na sledniot jazol koga e ispolnet uslovot 
+        //nema potreba da od current = current->sled zatoa shto togash current go pomestuvame za edno mesto na desno
       }
     }
   } while (swapped);
@@ -124,31 +123,26 @@ void sort(listi4ka &l) {
 int main(){
   listi4ka l;
   int n,m,p;
-  bool eb=true;
+  bool eb;
   l.init();
   cin>>n>>m;
   l.pishiPrv(n);
-  p=m-1;
+  if((m>n || m<n)&& !cin.fail())eb=true;//ako ova e ispolneto znaci deka se vneseni barem 2 validni clena
   while (true)
   {
-    if (m>n && m>p)
+    if ((m>n || m>p) && !cin.fail()) //tuka e cin.fail za da prekine vnesuvanjeto koga ke se vnese karakter
+    {
+      p=m;
+      l.pishiPosl(m);
+      cin>>m;
+    }else if (m<p && !cin.fail())
     {
       p=m;
       l.pishiPosl(m);
       cin>>m;
     }else break;
   }
-  if (p!=m)p=m+2;
-  while (true)
-  {
-    if (m<p)
-    {
-      p=m;
-      l.pishiPosl(m);
-      cin>>m;
-    }else break;
-  }
-  l.pechatiLista();
-  sort(l);
+  
+  if(eb)sort(l);
   l.pechatiLista();
 }
