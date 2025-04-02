e7519ac28895b31e5770a56c72b5b6df4830c294
pluto <84376137+pluto1608@users.noreply.github.com>
2025-04-02 22:06:48 +0000

    copypasta dodadena sega e kompletna zadacata

--- labs32.cpp
+++ labs32.cpp
@@ -125,6 +125,18 @@ int main(){
   int n,m,p;
   bool eb;
   l.init();
+  cout<<"A list of the Italian brainrot characters(tralalelo tralala supremacy):Tralalelo Tralala,
+  Bombardino coccodrillo, Bombombini Gusini, Trippi Troppi, Burbaloni Luliloli,
+  Tracotocutulo Lirilì Larilà, Brr brr Patapim, Trulimero Trulicina, Bobrini Cocosini,
+  Frigo Camello, Frulli Frulla, La vaca saturno saturnita, Crocodildo Penisini,
+  Bobritto bandito, Giraffa Celeste, Cappuccino Assassino, Glorbo Fruttodrillo, Camelrino Tazzino,
+  Ambatron, Kaktus tus tus kutus kutus, Mic Sahur, Her Tehaer, Mie ayam, Mubajir nasinya,
+  Beduk dug dug, Pat pat ketupat, Polisi sok asik, Ten ten tentara, Tuyuh tuyuh tuyuh,
+  Kur kur Kukuruyuk, Hi hi hi hi hiu, Karpet masjid, Monyet pura pura puasa, Pengajak Mokel,
+  Boneca Ambalabu, Tang tang tang bayar hutang kau, Bis Hitam, Tralaluli Patraluli, Hamster mata merah,
+  Pencil Dud dud gendud, Kaktus Tidak Taraweh, Pulpen biru, Pohon pisang, Monyet azan, Cik cik cikal cik,
+  Hor Hor Hor Horeg, Sahur Puasa tapi tidak sholat 5 waktu, Bombinarium, Nerpinarium,
+  El Bobrito de Kurvito, El szczurito kurwito golfito"<<endl;
   cin>>n>>m;
   l.pishiPrv(n);
   if((m>n || m<n)&& !cin.fail())eb=true;//ako ova e ispolneto znaci deka se vneseni barem 2 validni clena
